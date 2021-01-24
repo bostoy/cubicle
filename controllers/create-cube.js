fec80ec0e0b746b62447c338d65dd60be346dcdc
Borislav Stoyanov <bostoycontact@gmail.com>
2021-01-24 19:00:06 +0200

    init

--- controllers/create-cube.js
+++
@@ -1,4 +0,0 @@
-const Cube = require('../models/cube')
-
-const newCube = new Cube('default', 'this is a default cube', 'https://google.com', 1)
-newCube.save()
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const env = process.env.NODE_ENV || 'development';
 
 const config = require('./config/config')[env];
-const express = require('express');
-
-const app = express();
-
-
+const app = require('express')();
 
 require('./config/express')(app);
-require('./routes')(app)
+require('./config/routes')(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`))
+app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
