4cdf72ca8f64314ff687a01d860ca1df44082e68
Borislav Stoyanov <bostoycontact@gmail.com>
2021-01-25 17:49:52 +0200

    cube-partial

--- routes/router.js
+++ routes/router.js
@@ -3,26 +3,36 @@ const router = express.Router()
 
 router.route('/')
     .get((req, res, next) => {
-        res.render('index.hbs')
+        res.render('index.hbs', {
+            title: 'Home | Cubicle Workshop'
+        })
     })
 
 router.route('/about')
     .get((req, res, next) => {
-        res.render('about.hbs')
+        res.render('about.hbs', {
+            title: 'About | Cubicle Workshop'
+        })
     })
 
 router.route('/create')
     .get((req, res, next) => {
-        res.render('create.hbs')
+        res.render('create.hbs', {
+            title: 'Create | Cubicle Workshop'
+        })
     })
 
 router.route('/details/:id')
     .get((req, res, next) => {
-        res.render('details.hbs')
+        res.render('details.hbs', {
+            title: "Details | Cubicle Workshop"
+        })
     })
 
 router.route('*')
     .all((req, res) => {
-        res.render('404.hbs')
+        res.render('404.hbs', {
+            title: 'Error | Cubicle Workshop'
+        })
     })
 module.exports = router
