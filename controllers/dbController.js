f7ca94613049906a80fa99251b14d033f251df88
Borislav Stoyanov <bostoycontact@gmail.com>
2021-01-26 14:57:54 +0200

    db-filter-function

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -4,8 +4,23 @@ const fs = require('fs')
 const readDB = function () {
     return JSON.parse(fs.readFileSync('./db/database.json'))
 }
+const searchDB = function (name, difficultyFrom, difficultyTo) {
+    const resultArr = []
+    const db = readDB()
 
+    db.forEach(cube => {
+        if (cube.name.includes(name) && Number(cube.difficulty) >= Number(difficultyFrom) && Number(cube.difficulty) <= Number(difficultyTo)) {
+            resultArr.push(cube)
+        }
+    });
+
+    return resultArr
+}
+
+searchDB('Eco', '0', '9')
 
 module.exports = {
-    readDB
+    readDB,
+    searchDB,
+
 }
